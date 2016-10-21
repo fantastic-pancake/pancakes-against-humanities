be1b1d5774aecf22b5388cb459108bdbbc0d1073
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-10-21 16:04:25 -0500

    03.04 Eslint Config

--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,21 @@ console.log(`hello ${blegh}`);
 const obj = {hey: 1};
 const obj2 = {...obj, blegh: 2};
 console.log(obj2);
+
+class AppComponent {
+	static PropTypes = {
+		blegh: "whoa"
+	};
+}
+
+switch (blegh) {
+	case 1:
+		console.log("HEY");
+		break;
+		
+	case 2:
+		console.log("WHOA");
+		break;
+}
+
+console.log(<AppComponent />);

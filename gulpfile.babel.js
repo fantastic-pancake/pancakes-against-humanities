2bcab5aa97a753acee8dc77052f68409189afc49
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-10-21 17:24:33 -0500

    03.07 Server Watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,12 +11,42 @@ gulp.task("server:clean", cb => {
 gulp.task("server:build",
 	gulp.series(
 		"server:clean",
-		() => {
-			return gulp.src("./src/server/**/*.js")
-				.pipe($.changed("./build"))
-				.pipe($.sourcemaps.init())
-				.pipe($.babel())
-				.pipe($.sourcemaps.write(".", {sourceRoot: path.join(__dirname, "src", "server")}))
-				.pipe(gulp.dest("./build"));
-		}
+		compileServer
 	));
+
+gulp.task(
+	"server:watch",
+	gulp.series(
+		"server:build",
+		watchServer
+	));
+
+gulp.task(
+	"server:dev",
+	gulp.series(
+		"server:build",
+		gulp.parallel(
+			watchServer,
+			function nodemon() {
+				return $.nodemon({
+					script: "./server.js",
+					watch: "build"
+				});
+			}
+		)
+	));
+
+function compileServer() {
+	return gulp.src("./src/server/**/*.js")
+		.pipe($.changed("./build"))
+		.pipe($.sourcemaps.init())
+		.pipe($.babel())
+		.pipe($.sourcemaps.write(".", {sourceRoot: path.join(__dirname, "src", "server")}))
+		.pipe(gulp.dest("./build"));
+}
+
+function watchServer() {
+	return gulp
+		.watch("./src/server/**/*.js", gulp.series(compileServer))
+		.on("error", () => {});
+}

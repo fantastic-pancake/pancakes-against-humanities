a3d6d30701f45424281c56ada91cd1426f9959bb
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-10-21 18:34:29 -0500

    03.08 Server Tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import path from "path";
 import rimraf from "rimraf";
+import child_process from "child_process";
 
 const $ = require("gulp-load-plugins")();
 
@@ -27,14 +28,25 @@ gulp.task(
 		"server:build",
 		gulp.parallel(
 			watchServer,
-			function nodemon() {
-				return $.nodemon({
-					script: "./server.js",
-					watch: "build"
-				});
-			}
+			runServer
 		)
 	));
+
+gulp.task(
+	"server:test",
+	gulp.series(
+		"server:build",
+		testServer
+	));
+
+gulp.task(
+	"server:test:dev",
+	gulp.series(
+		"server:build",
+		gulp.parallel(
+			watchServer,
+			runServerTests
+		)));
 
 function compileServer() {
 	return gulp.src("./src/server/**/*.js")
@@ -49,4 +61,32 @@ function watchServer() {
 	return gulp
 		.watch("./src/server/**/*.js", gulp.series(compileServer))
 		.on("error", () => {});
+}
+
+function runServer() {
+	return $.nodemon({
+		script: "./server.js",
+		watch: "build",
+		ignore: ["**/__tests"]
+	});
+}
+
+function testServer(cb) {
+	child_process.exec("node ./tests.js", (err, stdout, stderr) => {
+		console.log(stdout);
+		console.error(stderr);
+
+		if (err) {
+			cb(new $.util.PluginError("testServer", "Tests failed"));
+		} else {
+			cb();
+		}
+	});
+}
+
+function runServerTests() {
+	return $.nodemon({
+		script: "./tests.js",
+		watch: "build"
+	});
 }

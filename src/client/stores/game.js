fa156d7128e8a7f5d6ec4de31f85f12be51a8dda
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-11-05 18:01:02 -0500

    04.26 Card Component

--- src/client/stores/game.js
+++ src/client/stores/game.js
@@ -12,16 +12,16 @@ const defaultView = {
 		sets: ["1ed"]
 	},
 	players: [
-		{id: 1, name: "Nelson", score: 3, isCzar: false, isPlaying: false, isWinner: true},
+		{id: 1, name: "Nelson", score: 3, isCzar: false, isPlaying: true, isWinner: false},
 		{id: 2, name: "Blegh", score: 1, isCzar: false, isPlaying: true, isWinner: false},
 		{id: 3, name: "Whoa", score: 4, isCzar: true, isPlaying: false, isWinner: false},
 		{id: 4, name: "Stuff", score: 2, isCzar: false, isPlaying: false, isWinner: false}
 	],
 	messages: [
-		{name: "Nelson", message: "Blegh"},
-		{name: "Nelson", message: "Blegh"},
-		{name: "Nelson", message: "Blegh"},
-		{name: "Nelson", message: "Blegh"}
+		{index: 1, name: "Nelson", message: "Blegh"},
+		{index: 2, name: "Nelson", message: "Blegh"},
+		{index: 3, name: "Nelson", message: "Blegh"},
+		{index: 4, name: "Nelson", message: "Blegh"}
 	],
 	round: {
 		blackCard: {
@@ -41,21 +41,20 @@ const defaultView = {
 
 const defaultPlayerView = {
 	id: 1,
-	hand: [],
+	hand: [
+		{id: 2, text: "Card 1", set: "1ed"},
+		{id: 3, text: "Card 2", set: "1ed"},
+		{id: 4, text: "Card 3", set: "1ed"},
+		{id: 5, text: "Card 4", set: "1ed"},
+		{id: 7, text: "Card 6", set: "1ed"},
+		{id: 8, text: "Card 7", set: "1ed"},
+		{id: 9, text: "Card 8", set: "1ed"},
+		{id: 10, text: "Card 9", set: "1ed"},
+		{id: 11, text: "Card 10", set: "1ed"}
+	],
 	stack: {
 		id: 2,
-		cards: [
-			{id: 2, text: "Card 1", set: "1ed"},
-			{id: 3, text: "Card 2", set: "1ed"},
-			{id: 4, text: "Card 3", set: "1ed"},
-			{id: 5, text: "Card 4", set: "1ed"},
-			{id: 6, text: "Card 5", set: "1ed"},
-			{id: 7, text: "Card 6", set: "1ed"},
-			{id: 8, text: "Card 7", set: "1ed"},
-			{id: 9, text: "Card 8", set: "1ed"},
-			{id: 10, text: "Card 9", set: "1ed"},
-			{id: 11, text: "Card 10", set: "1ed"}
-		]
+		cards: [{id: 6, text: "Card 5", set: "1ed"}]
 	}
 };
 

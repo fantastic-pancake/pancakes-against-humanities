812228b935109a379a6d1d546081bb7e9f710e39
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-11-04 21:21:03 -0500

    04.15 Print Action

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,6 @@
 import "./client.scss";
 
+import _ from "lodash";
 import React from "react";
 import ReactDOM from "react-dom";
 import {Router, browserHistory as history} from "react-router";
@@ -13,6 +14,10 @@ import createStores from "./stores";
 // Services
 const dispatcher = new Dispatcher();
 const services = {dispatcher};
+
+if (IS_DEVELOPMENT) {
+	dispatcher.on("*", printAction);
+}
 
 // ----------------------------
 // Stores
@@ -42,3 +47,24 @@ if (module.hot) {
 // ----------------------------
 // Go!
 main();
+
+// ----------------------------
+// Helpers
+function printAction(action) {
+	if (action.hasOwnProperty("status")) {
+		let style = null;
+		switch (action.status) {
+			case A.STATUS_REQUEST: style = "color: blue"; break;
+			case A.STATUS_FAIL: style = "color: red"; break;
+			case A.STATUS_SUCCESS: style = "color: green"; break;
+		}
+
+		console.log(`%c${action.type}`, `${style}; font-weight: bold; background: #eee; width: 100%; display: block;`);
+	} else {
+		console.log(`%c${action.type}`, "background: #ddd");
+	}
+
+	const result = _.omit(action, ["type", "status"]);
+	if (_.keys(result).length)
+		console.log(result);
+}

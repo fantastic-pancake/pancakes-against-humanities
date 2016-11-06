aeae7d8d1362029be2210fcb87569e28e4ec2c7c
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-11-05 20:41:04 -0500

    05.06 Merge

--- src/server/shared/__tests/diff.js
+++ src/server/shared/__tests/diff.js
@@ -123,9 +123,14 @@ function runDiffTests(tests) {
 				it("diff", () => {
 					expect(result).toEqual(diff);
 				});
-			});
 
-			// TODO: ADD MERGE TEST
+				if (result != IS_UNCHANGED) {
+					it("merge", () => {
+						const mergedBack = mergeDiff(before, result);
+						expect(mergedBack).toEqual(after);
+					});
+				}
+			});
 		} else if (_.isObject(test)) {
 			describe(`${key}:`, () => runDiffTests(test));
 		}

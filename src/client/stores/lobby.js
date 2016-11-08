deb9b90e5dd0ee6041c9f31b9423e7991ffbf971
Nelson LaQuet <nelsonlaquet@gmail.com>
2016-11-07 21:18:10 -0600

    05.10 Lobby

--- src/client/stores/lobby.js
+++ src/client/stores/lobby.js
@@ -2,31 +2,20 @@ import {Observable} from "rxjs";
 import {Validator} from "shared/validation";
 import {validateMessage} from "shared/validation/chat";
 import {mapOp$} from "shared/observable";
+import {createView$} from "../lib/stores";
 import * as A from "../actions";
 
 const defaultView = {
-	messages: [
-		{index: 1, name: "Person", message: "dsadsad adsdadasdas"},
-		{index: 2, name: "Person", message: "Blegh"},
-		{index: 3, name: "Whoa", message: "dsadsa"},
-		{index: 4, name: "Person", message: "Bdasdas sad asdaslegh"},
-		{index: 5, name: "Person", message: "d asdsad asdas das"}
-	],
-
-	games: [
-		{title: "Game 1", id: 1, players: ["one", "two", "three"]},
-		{title: "Game 2", id: 2, players: ["one", "two", "three"]},
-		{title: "Game 3", id: 3, players: ["one", "two", "three"]},
-		{title: "Game 4", id: 4, players: ["one", "two", "three"]}
-	]
+	messages: [],
+	games: []
 };
 
 export default class LobbyStore {
-	constructor({dispatcher}, user) {
-		this.view$ = Observable.of(defaultView);
+	constructor({dispatcher, socket}, user) {
+		this.view$ = createView$(dispatcher, A.VIEW_LOBBY, defaultView);
 
 		dispatcher.onRequest({
-			[A.LOBBY_JOIN]: action => dispatcher.succeed(action),
+			[A.LOBBY_JOIN]: action => socket.emit("action", action),
 
 			[A.LOBBY_SEND_MESSAGE]: action => {
 				const validator = new Validator();
@@ -40,7 +29,7 @@ export default class LobbyStore {
 					return;
 				}
 
-				// TODO: SEND TO SOCKET
+				socket.emit("action", action);
 			}
 		});
 

10584729661881d84be770a0b965b4e3a61cf453
phc5 <paulhyunchong@gmail.com>
2017-01-11 09:19:41 -0800

    add back-to-home button for nav

--- src/client/components/home/home.js
+++ src/client/components/home/home.js
@@ -9,7 +9,7 @@ class Home extends Component {
 	render() {
 		return (
 			<section className="home-container">
-				<div className="center">
+				<div className="center title-container">
 					<h1 className="title">Cards Against Humanity</h1>
 					<button><a href="#/game">Log In</a></button>
 					<button><a href="#/rules">How to Play</a></button>

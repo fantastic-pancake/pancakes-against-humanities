44dc1dbb751efc7ada34e94e7e86773b78ef83a4
phc5 <paulhyunchong@gmail.com>
2017-01-11 12:35:10 -0800

    add score and timer components

--- src/client/components/game/game.js
+++ src/client/components/game/game.js
@@ -2,6 +2,8 @@ import "./game.scss";
 import React, {Component} from "react";
 import BlackCardContainer from './black-card-container/black-card-container';
 import WhiteCardContainer from './white-card-container/white-card-container';
+import Timer from './timer/timer';
+import Score from './score/score';
 
 class Game extends Component {
 	componentDidMount() {
@@ -12,6 +14,10 @@ class Game extends Component {
 		return (
 			<section className="game-container">
 				<div className="center">
+					<div className="game-data">
+						<Score />
+						<Timer secondsRemaining="30"/>
+					</div>
 					<a href="#/"><button className="nav">Back to Home</button></a>
 					<BlackCardContainer />
 					<WhiteCardContainer />
---
+++ src/client/components/game/timer/timer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+var Timer = React.createClass({
+	getInitialState: function() {
+		return {
+			secondsRemaining: 0
+		};
+	},
+	tick: function() {
+		this.setState({secondsRemaining: this.state.secondsRemaining - 1});
+		if (this.state.secondsRemaining <= 0) {
+			clearInterval(this.interval);
+		}
+	},
+	componentDidMount: function() {
+		this.setState({ secondsRemaining: this.props.secondsRemaining });
+		this.interval = setInterval(this.tick, 1000);
+	},
+	componentWillUnmount: function() {
+		clearInterval(this.interval);
+	},
+	render: function() {
+		return (
+			<div className="timer">
+				Seconds Remaining: {this.state.secondsRemaining}
+			</div>
+		);
+	}
+});
+
+export default Timer;

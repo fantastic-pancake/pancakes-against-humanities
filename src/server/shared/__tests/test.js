b0758815528d222ad198eb55f960df1e3aac5841
Benjamin Soung <soungbenjamin@gmail.com>
2017-01-11 10:46:27 -0500

    update test.js

--- src/server/shared/__tests/test.js
+++ src/server/shared/__tests/test.js
@@ -1,4 +1,37 @@
+console.log('Server side: ')
+
 describe("Something", () => {
+	it("works", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("User authentication", () => {
+	it("redirects user to oauth", () => expect(true).toBe(true));
+	it("redirects user to oauth", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("Start game", () => {
+	it("works", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("Join game", () => {
+	it("works", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("Distribute cards to user", () => {
+	it("works", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("User selects card", () => {
+	it("works", () => expect(true).toBe(true));
+	it("doesn't work", () => expect(true).toBe(true));
+});
+
+describe("Live chat", () => {
 	it("works", () => expect(true).toBe(true));
 	it("doesn't work", () => expect(true).toBe(true));
 });

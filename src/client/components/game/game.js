98cf1737b72042b6c9d88835497f265761fe3efb
phc5 <paulhyunchong@gmail.com>
2017-01-11 08:15:42 -0800

    split up white card section into smaller components and add animatino

--- src/client/components/game/game.js
+++ src/client/components/game/game.js
@@ -1,54 +1,26 @@
 import "./game.scss";
 import React, {Component} from "react";
+import WhiteCardContainer from './white-card-container/white-card-container';
 
-class Home extends Component {
+class Game extends Component {
 	componentDidMount() {
-		console.log("IN GAME");
+		
 	}
 
 	render() {
 		return (
 			<section className="game-container">
-				<div className="black-card-container">
-					<div>
-						What did Vin Diesel eat for dinner?
+				<div className="center">
+					<div className="black-card-container">
+						<div>
+							What did Vin Diesel eat for dinner?
+						</div>
 					</div>
-				</div>
-				<div className="white-card-container">
-					<div className="white-card">
-						Geese.
-					</div>
-					<div className="white-card">
-						Doo-doo.
-					</div>
-					<div className="white-card">
-						Police brutality.
-					</div>
-					<div className="white-card">
-						Hillary Clinton&#8217;s death stare.
-					</div>
-					<div className="white-card">
-						My soul.
-					</div>
-					<div className="white-card">
-						Beefin&#8217; over turf.
-					</div>
-					<div className="white-card">
-						Teenage pregnancy.
-					</div>
-					<div className="white-card">
-						Becoming a blueberry.
-					</div>
-					<div className="white-card">
-						The Force.
-					</div>
-					<div className="white-card">
-						Sweet, sweet vengeance.
-					</div>
+					<WhiteCardContainer />
 				</div>
 			</section>
 		);
 	}
 }
 
-export default Home;
+export default Game;

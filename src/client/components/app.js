ffd53baf27c7ac1418f6cc0ad9143721172d9a67
jpke <j.patrick.earnest@gmail.com>
2017-01-17 15:38:00 -0500

    "background styling"

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,10 +1,7 @@
 import React from 'react';
-// import io from 'socket.io-client';
 
 export default React.createClass({
 	componentWillMount() {
-		// var socket = io.connect();
-		// this.setState({socket:socket});
 	},
 	render() {
 		return (
